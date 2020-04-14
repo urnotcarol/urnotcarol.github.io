fd53e8f03cfd6e5cf64688e7a9775e5b86c5af02
caorong <caorong001@ke.com>
2020-04-14 09:53:28 +0800

    build

---
+++ precache-manifest.7b59839c60c00af6596fd34bfb6ad655.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "99ce095c384238a55a7f",
+    "url": "/static/js/main.99ce095c.chunk.js"
+  },
+  {
+    "revision": "a3d1bb320c4b7e7e8e07",
+    "url": "/static/js/1.a3d1bb32.chunk.js"
+  },
+  {
+    "revision": "99ce095c384238a55a7f",
+    "url": "/static/css/main.12f1b1d0.chunk.css"
+  },
+  {
+    "revision": "a3d1bb320c4b7e7e8e07",
+    "url": "/static/css/1.3f63b426.chunk.css"
+  },
+  {
+    "revision": "13ed4b8bf096766fc2c3e492e33330be",
+    "url": "/index.html"
+  }
+];
